caa5712b65099558e06b28e295794b9c19140d9e
shikha06 <37832720+shikha06@users.noreply.github.com>
2018-04-07 20:35:04 +0530

    Update initial.cpp

--- initial.cpp
+++ initial.cpp
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include <limits.h>
 
-#define MIN_PID 500
-#define MAX_PID 3000
+#define MIN_PID 100
+#define MAX_PID 1000
 #define cb CHAR_BIT
 
 int sz = MAX_PID - MIN_PID + 1;
 
 unsigned char *b;
 
-int allocate_map();
+int allocate_mp();
 int allocate_pid();
 void release_pid(int pid);
